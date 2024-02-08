569831a5895d7f03a7c76705ee2d21635a02e4c8
tadeucassis-dev <tadeu.cesar@worldconnectoficial.com>
2024-02-08 14:29:44 -0300

    update code

---
+++ src/pages/index.tsx
@@ -0,0 +1,55 @@
+import Head from "next/head"
+import Image from 'next/image'
+import { Box } from "@chakra-ui/react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import styles from '../styles/home.module.scss'
+import acainokilo from '../assets/acainokilo.png'
+
+export default function Home() {
+  return (
+
+    <>
+      <Box
+       minHeight={'100vh'}
+       display={'flex'}
+       justifyContent={'center'}
+       alignItems={'center'}
+       flexDirection={'column'}
+       backgroundColor={'#8B008B'}
+      >
+        <Head>
+          <title>Açai no kilo - Faça seu login</title>
+        </Head>
+        <div className={styles.containerCenter}>
+          <Image src={acainokilo} alt="Logo Sujeito Pizzaria" />
+
+          <div className={styles.login}>
+            <form>
+
+              <Input
+                placeholder='Digite seu email'
+                type="text"
+              />
+
+              <Input
+                placeholder='Digite sua senha'
+                type="password"
+              />
+
+              <Button
+                type='submit'
+                loading={false}
+              >
+                Acessar
+              </Button>
+            </form>
+
+            <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+
+          </div>
+        </div>
+      </Box>
+    </>
+  );
+}
